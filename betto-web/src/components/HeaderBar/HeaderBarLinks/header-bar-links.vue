e0efcb7b5f456dc56ea0281763e48a518f98dae9
bulchsu <aleksander.jan.stelmach@gmail.com>
2020-03-21 18:17:17 +0100

    vue-project-created

---
+++ betto-web/src/components/HeaderBar/HeaderBarLinks/header-bar-links.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a :href="github" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      github: 'https://www.google.com/'
+    };
+  },
+};
+</script>
---
+++ betto-web/src/components/HeaderBar/header-bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav id="navbar">
+      <HeaderBarBrand></HeaderBarBrand>
+      <HeaderBarLinks></HeaderBarLinks>
+  </nav>
+</template>
+
+<script>
+import HeaderBarLinks from '@/components/HeaderBar/HeaderBarLinks/header-bar-links';
+import HeaderBarBrand from '@/components/HeaderBar/HeaderBarBrand/header-bar-brand';
+
+export default {
+  name: 'HeaderBar',
+  components: { HeaderBarBrand, HeaderBarLinks },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/_colors.scss";
+
+#navbar {
+  background-color: $green-background;
+  width: 100%;
+  height: 265px;
+}
+
+</style>
