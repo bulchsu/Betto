2b18b652253f33bc4e77c98d6c6a30fe2b9af60e
bulchsu <aleksander.jan.stelmach@gmail.com>
2020-03-22 17:05:48 +0100

    Add vuetify

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <v-app>
     <HeaderBar />
+    <SideBar />
   </v-app>
 </template>
 
 <script>
 import HeaderBar from '@/components/HeaderBar/header-bar';
+import SideBar from '@/components/SideBar/side-bar'
 
 export default {
   name: "app",
   components: {
-    HeaderBar
+    HeaderBar, SideBar
   }
 };
 </script>
