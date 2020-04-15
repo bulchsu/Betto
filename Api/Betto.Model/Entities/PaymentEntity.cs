6cff2b9a4f6f8d82eb6f70fdd979b249b98f9cb3
bulchsu <aleksander.jan.stelmach@gmail.com>
2020-04-15 14:32:50 +0200

    Add payment entity

---
+++ Api/Betto.Model/Entities/PaymentEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Betto.Model.Models;
+
+namespace Betto.Model.Entities
+{
+    public class PaymentEntity
+    {
+        [Key]
+        public int PaymentId { get; set; }
+        public UserEntity User { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public double Amount { get; set; }
+        public PaymentTypeEnum Type { get; set; }
+        public DateTime PaymentDateTime { get; set; }
+    }
+}
